bc0f2518fefe3fdbf5c0ca49294808dc665a02c7
Victor M. Cortes Figueroa <vmcortesf@gmail.com>
2025-08-13 18:15:44 -0500

    Lesson 20: Next/Link
    - https://www.udemy.com/course/nextjs-fh/learn/lecture/30576968#learning-tools
    
    Update Navbar component to use links and add icon; update dependencies
    
    - Refactored Navbar component to replace static links with dynamic ones using an array for better maintainability.
    - Added HomeIcon from '@primer/octicons-react' for improved UI.
    - Updated package.json and package-lock.json to include the new dependency.

--- 01-first-steps/components/navbar/Navbar.tsx
+++ 01-first-steps/components/navbar/Navbar.tsx
@@ -1,19 +1,28 @@
+import { HomeIcon } from '@primer/octicons-react';
+import Link from 'next/link';
+
+const navItems = [
+  { path: '/', text: 'Home' },
+  { path: '/about', text: 'About' },
+  { path: '/pricing', text: 'Pricing' },
+  { path: '/contact', text: 'Contact' },
+];
+
 export const Navbar = () => {
   return (
     <nav className='flex bg-blue-800 bg-opacity-30 p-2 m-2 rounded'>
-      <span className='text-white'>Home</span>
+      <Link href={'/'} className='flex items-center mr-2 text-white'>
+        <HomeIcon className='mr-2' />
+        <span className='text-white'>Home</span>
+      </Link>
 
       <div className='flex flex-1'></div>
 
-      <a className='mr-2 text-white' href='/about'>
-        About
-      </a>
-      <a className='mr-2 text-white' href='/pricing'>
-        Pricing
-      </a>
-      <a className='mr-2 text-white' href='/contact'>
-        Contact
-      </a>
+      {navItems.map((item) => (
+        <Link key={item.path} className='mr-2 text-white' href={item.path}>
+          {item.text}
+        </Link>
+      ))}
     </nav>
   );
 };
